01254ed568559c083c4a4ed386c1dd892fde42f1
alpakash <akash.soedamah@gmail.com>
2025-02-22 03:37:10 +0100

    fix

--- src/components/common/SEO/index.jsx
+++ src/components/common/SEO/index.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import Helmet from 'react-helmet';
+import { Helmet } from 'react-helmet-async';
 // import Thumbnail from 'assets/thumbnail/thumbnail.png';
 import {
   url,
